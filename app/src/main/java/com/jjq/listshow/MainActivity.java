6bcc9a0c485b19dfe4cc8661b60a4eabf44511d8
jieqiang <1035288257jjq@gmail.com>
2016-10-17 14:56:02 +0800

    first commit

---
+++ app/src/main/java/com/jjq/listshow/MainActivity.java
@@ -0,0 +1,20 @@
+package com.jjq.listshow;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button1, button2, button3, button4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
+        button4 = (Button)findViewById(R.id.button4);
+    }
+}
